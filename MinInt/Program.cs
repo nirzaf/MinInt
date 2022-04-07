1e893eac79bcf026368bb9180358f67b0eaef73f
Fazrin <mfmfazrin1986@gmail.com>
2022-04-07 17:15:01 +0530

    Add project files.

---
+++ MinInt/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var randoms = new List<object>()
+    {23, "33", "ttt", 3.5, 2.0, 0, "-100", new object(), ...}
+{"ttt", new object();
+}
+
+
+public static class Util
+{
+    public string MinValue(List<object> valObj)
+    {
+        List<object> newObj = new List<object>();
+        for (int i = 0; i < valObj.Count; i++)
+        {
+            //Sort all 
+            if (int.TryParse(valObj[i].ToString(), out int ValidInt))
+            {
+                newObj.Add(valObj[i]);
+            }
+
+            //sort list of newObj
+
+
+
+            //get first index sorted list
+
+            return 
+        }
+    }
+}
